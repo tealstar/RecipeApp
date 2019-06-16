4249c87d1e3cb57d23813864f264a0b5da11ab77
tealstar <mary03302004@gmail.com>
2019-06-15 19:18:24 -0800

    two pane done

--- app/src/main/java/com/example/recipeapp/UIScreen2Components/StepAndIngredientsActivity.java
+++ app/src/main/java/com/example/recipeapp/UIScreen2Components/StepAndIngredientsActivity.java
@@ -11,6 +11,7 @@ import com.example.recipeapp.Model.IngredientsList;
 import com.example.recipeapp.Model.StepsList;
 import com.example.recipeapp.R;
 import com.example.recipeapp.UIScreen3Components.StepDetails;
+import com.example.recipeapp.UIScreen3Components.StepDetailsFragment;
 
 import java.util.ArrayList;
 
@@ -21,6 +22,9 @@ public class StepAndIngredientsActivity extends AppCompatActivity implements
     private String recipeServings;
     private ArrayList<IngredientsList> ingredientsLists;
     private ArrayList<StepsList> stepsLists;
+    private Boolean mTwoPane;
+    FragmentManager fragmentManager;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,31 +34,39 @@ public class StepAndIngredientsActivity extends AppCompatActivity implements
         ingredientsLists = new ArrayList<>();
         stepsLists = new ArrayList<>();
 
+        fragmentManager = getSupportFragmentManager();
+        bundle = new Bundle();
+
+        if (findViewById(R.id.tablet_view_step_detail_layout) != null) {
+            mTwoPane = true;
+        } else {
+            mTwoPane = false;
+        }
+
         getRecipeInformation();
     }
 
-    private void getRecipeInformation(){
-        if(getIntent().hasExtra(Constants.SEND_RECIPE_NAME)){
+    private void getRecipeInformation() {
+        if (getIntent().hasExtra(Constants.SEND_RECIPE_NAME)) {
             recipeName = getIntent().getStringExtra(Constants.SEND_RECIPE_NAME);
         }
-        if(getIntent().hasExtra(Constants.SEND_RECIPE_SERVINGS)){
+        if (getIntent().hasExtra(Constants.SEND_RECIPE_SERVINGS)) {
             recipeServings = getIntent().getStringExtra(Constants.SEND_RECIPE_SERVINGS);
         }
-        if(getIntent().hasExtra(Constants.SEND_RECIPE_INGREDIENTS_LIST)){
+        if (getIntent().hasExtra(Constants.SEND_RECIPE_INGREDIENTS_LIST)) {
             ingredientsLists = getIntent().getParcelableArrayListExtra(Constants.SEND_RECIPE_INGREDIENTS_LIST);
         }
 
-        if(getIntent().hasExtra(Constants.SEND_RECIPE_STEPS_LIST)){
+        if (getIntent().hasExtra(Constants.SEND_RECIPE_STEPS_LIST)) {
             stepsLists = getIntent().getParcelableArrayListExtra(Constants.SEND_RECIPE_STEPS_LIST);
         }
+
         showRecipeInformation();
     }
 
-    private void showRecipeInformation(){
+    private void showRecipeInformation() {
 
         StepAndIngredientsFragment stepAndIngredientsFragment = new StepAndIngredientsFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Bundle bundle = new Bundle();
         bundle.putString(Constants.SEND_RECIPE_NAME, recipeName);
         bundle.putParcelableArrayList(Constants.SEND_RECIPE_INGREDIENTS_LIST, ingredientsLists);
         bundle.putParcelableArrayList(Constants.SEND_RECIPE_STEPS_LIST, stepsLists);
@@ -64,18 +76,31 @@ public class StepAndIngredientsActivity extends AppCompatActivity implements
         fragmentManager.beginTransaction()
                 .add(R.id.recipe_steps_frame_layout, stepAndIngredientsFragment)
                 .commit();
+
     }
 
     @Override
     public void onListItemClick(int position) {
 
-        StepsList stepsList = stepsLists.get(position);
-        String shortDescription = stepsList.getShortDescription();
-        String videoUrl = stepsList.getVideoURL();
+        StepsList stepListObject = stepsLists.get(position);
+        String shortDescription = stepListObject.getShortDescription();
+        String videoUrl = stepListObject.getVideoURL();
 
-        Intent intent = new Intent(StepAndIngredientsActivity.this, StepDetails.class);
-        intent.putExtra(Constants.SEND_RECIPE_SHORT_DESCRIPTION, shortDescription);
-        intent.putExtra(Constants.SEND_RECIPE_VIDEO_URL, videoUrl);
-        startActivity(intent);
+        if (mTwoPane == false) {
+            Intent intent = new Intent(StepAndIngredientsActivity.this, StepDetails.class);
+            intent.putExtra(Constants.SEND_RECIPE_SHORT_DESCRIPTION, shortDescription);
+            intent.putExtra(Constants.SEND_RECIPE_VIDEO_URL, videoUrl);
+            startActivity(intent);
+        }else {
+            StepDetailsFragment stepDetailsFragment = new StepDetailsFragment();
+            bundle.putString(Constants.SEND_RECIPE_SHORT_DESCRIPTION, shortDescription);
+            bundle.putString(Constants.SEND_RECIPE_VIDEO_URL, videoUrl);
+            stepDetailsFragment.setArguments(bundle);
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.step_details_frame_layout, stepDetailsFragment)
+                    .commit();
+        }
     }
-}
+
+    }
